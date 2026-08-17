6f0fed4d81b95650033064eec92e9879e388e381
farhad1217 <gle.farhad@gmail.com>
2026-08-18 04:27:25 +0600

    Dynamic the logic(play, pause, reverse & restart) & gained advanced knowledge about GSAP timeline

--- src/components/gsap_timeline.jsx
+++ src/components/gsap_timeline.jsx
@@ -1,17 +1,27 @@
 import gsap from "gsap"
 import { useGSAP } from "@gsap/react"
+import { useRef, useState } from "react"
 
 export default function GsapTimeline(){
+    const [isPlaying, setIsPlaying] = useState(true)
+    const boxTl = useRef(null)
 
     useGSAP(()=>{
-        const play = document.querySelector(".play")
-        const pause = document.querySelector(".pause")
-        const reverse = document.querySelector(".reverse")
-        const restart = document.querySelector(".restart")
 
-        const boxTl = gsap.timeline({paused: false})
+        boxTl.current = gsap.timeline({
+            paused: true, 
+            repeat: -1, 
+            yoyo: true,
+            onUpdate: ()=> {
+                if(boxTl.current.time() === 0){
+                    setIsPlaying(true) 
+                    boxTl.current.restart()
+                    boxTl.current.pause()
+                }
+            }
+        })
 
-        boxTl.to(".box-1", {
+        boxTl.current.to(".box-1", {
             x: 500,
             duration: 2,
             ease: "power3.out"
@@ -24,32 +34,51 @@ export default function GsapTimeline(){
             duration: 2,
             ease: "power3.out"
         })
-
-        play.addEventListener("click", ()=>{
-            boxTl.play()
-        })
-
-        pause.addEventListener("click", ()=>{
-            boxTl.pause()
-        })
-
-        reverse.addEventListener("click", ()=>{
-            boxTl.reverse()
-        })
-
-        restart.addEventListener("click", ()=>{
-            boxTl.restart()
-        })
     })
     
 
+    function handlePlayPause(){
+        if(isPlaying){
+            if(boxTl.current.reversed()){
+                boxTl.current.reverse()
+                setIsPlaying(!isPlaying)
+            }
+            else{
+                boxTl.current.play()
+                setIsPlaying(!isPlaying)
+            }
+        }
+        else{
+            boxTl.current.pause()
+            setIsPlaying(!isPlaying)
+        }
+    }
+
+    function handleReverse(){
+        if(boxTl.current.time() !== 0){
+            if(boxTl.current.reversed()){
+                boxTl.current.play()
+            }
+            else{
+                boxTl.current.reverse()
+            }
+            setIsPlaying(false)
+        }
+    }
+
+    function handleRestart(){
+        if(boxTl.current.time() !== 0){
+            boxTl.current.restart()
+            setIsPlaying(false) 
+        }
+    }
+
     return <>
-        <div className="box-1 w-[150px] h-[150px] font-bold bg-red-600 flex items-center justify-center mt-20">Box 1</div>
-        <div className="box-2 w-[150px] h-[150px] font-bold bg-green-600 flex items-center justify-center mt-20">Box 2</div>
-        <div className="w-[150px] h-[150px] font-bold bg-blue-600 flex items-center justify-center mt-20 box-3 mb-30">Box 3</div>
-        <button className="bg-white ml-20 p-2 rounded-lg play">play</button>
-        <button className="bg-white ml-20 p-2 rounded-lg pause">pause</button>
-        <button className="bg-white ml-20 p-2 rounded-lg reverse">reverse</button>
-        <button className="bg-white ml-20 p-2 rounded-lg restart">restart</button>
+        <div className="box-1 w-37.5 h-37.5 font-bold bg-red-600 flex items-center justify-center mt-20">Box 1</div>
+        <div className="box-2 w-37.5 h-37.5 font-bold bg-green-600 flex items-center justify-center mt-20">Box 2</div>
+        <div className="w-37.5 h-37.5 font-bold bg-blue-600 flex items-center justify-center mt-20 box-3 mb-30">Box 3</div>
+        <button onClick={handlePlayPause}  className="bg-white ml-20 p-2 rounded-lg playPause">{isPlaying? "Play": "Pause"}</button>
+        <button onClick={handleReverse} className="bg-white ml-20 p-2 rounded-lg reverse">reverse</button>
+        <button onClick={handleRestart} className="bg-white ml-20 p-2 rounded-lg restart">restart</button>
     </>
 }
