14ebdd617ca9c378473e592563e0ea20fc0430a8
farhad1217 <gle.farhad@gmail.com>
2026-08-30 02:13:14 +0600

    snapping at middle while scrolling

--- src/components/section_scroll_snap.jsx
+++ src/components/section_scroll_snap.jsx
@@ -8,9 +8,9 @@ export default function SectionScrollSnap(){
 
     useGSAP(()=>{
         const box = gsap.utils.toArray(".animation > div")
-        const left = box[0].getBoundingClientRect().left 
         const step = box[1].getBoundingClientRect().left - box[0].getBoundingClientRect().left
-        box.shift()
+        gsap.set(".animation", {x: -box[0].getBoundingClientRect().left + box[2].getBoundingClientRect().left})
+        box.pop()
         const tl = gsap.timeline({
             ease: "none",
             scrollTrigger: {
@@ -30,27 +30,27 @@ export default function SectionScrollSnap(){
         })
 
         tl.addLabel("box0")
-        .to(".animation", {x: -left})
 
-        box.forEach((_, i)=>{
-            tl.addLabel(`box${i+1}`)
-            .to(".animation", {x: `-=${step}`})
+        box.forEach((item, i)=>{
+            tl.to(".animation", {x: `-=${step}`})
+            .addLabel(`box${i+1}`)
         })
-
-        tl.addLabel("box5")
     })
 
     return <>
         <div className="screen1 w-full h-screen flex justify-center items-center">
             <div className="boxref">Scroll </div>
         </div>
-        <div className="screen2 w-full h-screen bg-black">
-            <div className="animation w-full h-screen flex justify-center items-center gap-20">
-                <div className="box1 w-60 h-60 bg-amber-500 relative">Box 1</div>
-                <div className="box2 w-60 h-60 bg-red-500 relative">Box 2</div>
-                <div className="box3 w-60 h-60 bg-green-500 relative">Box 3</div>
-                <div className="box4 w-60 h-60 bg-pink-500 relative">Box 4</div>
-                <div className="box5 w-60 h-60 bg-gray-500 relative">Box 5</div>
+        <div className="screen2 w-full h-screen bg-black overflow-x-hidden">
+            <div className="w-full h-screen flex justify-center items-center bg-transparent">            
+                <div className="animation w-full h-screen flex justify-center items-center gap-20">
+                    <div className="box1 w-60 h-60 bg-amber-500 relative">Box 1</div>
+                    <div className="box2 w-60 h-60 bg-red-500 relative">Box 2</div>
+                    <div className="box3 w-60 h-60 bg-green-500 relative">Box 3</div>
+                    <div className="box4 w-60 h-60 bg-pink-500 relative">Box 4</div>
+                    <div className="box5 w-60 h-60 bg-gray-500 relative">Box 5</div>
+                </div>
+                <div className="frame w-60 h-60 border-4 border-amber-50 fixed"></div>
             </div>
         </div>
     </>
