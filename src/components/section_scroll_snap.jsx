c8a73ccac73fdb7328e76bb9b65a860c80da011e
farhad1217 <gle.farhad@gmail.com>
2026-08-30 03:20:34 +0600

    snapping box into frame

--- src/components/section_scroll_snap.jsx
+++ src/components/section_scroll_snap.jsx
@@ -9,7 +9,9 @@ export default function SectionScrollSnap(){
     useGSAP(()=>{
         const box = gsap.utils.toArray(".animation > div")
         const step = box[1].getBoundingClientRect().left - box[0].getBoundingClientRect().left
-        gsap.set(".animation", {x: -box[0].getBoundingClientRect().left + box[2].getBoundingClientRect().left})
+        const frame = gsap.utils.toArray(".frame")
+        gsap.set(".animation", {x: -box[0].getBoundingClientRect().left + frame[0].getBoundingClientRect().left })
+        gsap.set(box[0], {scale: 0.7})
         box.pop()
         const tl = gsap.timeline({
             ease: "none",
@@ -22,19 +24,21 @@ export default function SectionScrollSnap(){
                 markers: true,
                 snap: {
                     snapTo: "labelsDirectional",
-                    duration: 0.1,
-                    ease: "none",
-                    delay: 0
+                    duration: 0.2,
                 }
             }
         })
 
         tl.addLabel("box0")
-
-        box.forEach((item, i)=>{
-            tl.to(".animation", {x: `-=${step}`})
+        box.forEach((_, i)=>{
+            tl
+            .to(".animation", {x: `-=${step}`})
+            .to(box[i+1], {scale: 0.7}, "<")
+            .to(box[i], {scale: 1}, "<")
             .addLabel(`box${i+1}`)
         })
+        const boxx = gsap.utils.toArray(".animation > div")
+        tl.to(boxx[boxx.length-1], {scale: 0.7}, "<")
     })
 
     return <>
@@ -44,11 +48,11 @@ export default function SectionScrollSnap(){
         <div className="screen2 w-full h-screen bg-black overflow-x-hidden">
             <div className="w-full h-screen flex justify-center items-center bg-transparent">            
                 <div className="animation w-full h-screen flex justify-center items-center gap-20">
-                    <div className="box1 w-60 h-60 bg-amber-500 relative">Box 1</div>
-                    <div className="box2 w-60 h-60 bg-red-500 relative">Box 2</div>
-                    <div className="box3 w-60 h-60 bg-green-500 relative">Box 3</div>
-                    <div className="box4 w-60 h-60 bg-pink-500 relative">Box 4</div>
-                    <div className="box5 w-60 h-60 bg-gray-500 relative">Box 5</div>
+                    <div className="box1 w-60 h-60 bg-amber-500 relative flex justify-center items-center text-5xl">Box 1</div>
+                    <div className="box2 w-60 h-60 bg-red-500 relative flex justify-center items-center text-5xl">Box 2</div>
+                    <div className="box3 w-60 h-60 bg-green-500 relative flex justify-center items-center text-5xl">Box 3</div>
+                    <div className="box4 w-60 h-60 bg-pink-500 relative flex justify-center items-center text-5xl">Box 4</div>
+                    <div className="box5 w-60 h-60 bg-gray-500 relative flex justify-center items-center text-5xl">Box 5</div>
                 </div>
                 <div className="frame w-60 h-60 border-4 border-amber-50 fixed"></div>
             </div>
